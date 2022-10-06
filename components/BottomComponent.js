5a77678f4ba3818c1ffe2cb67de721b59af8e1cb
mac <mac@MacBook-Air-de-mac.local>
2022-10-06 13:20:18 +0100

    finish project

--- components/BottomComponent.js
+++ components/BottomComponent.js
@@ -5,7 +5,7 @@ import { View, StyleSheet, Text } from 'react-native'
 const BottomComponent = () => {
   return (
     <View style={ styles.container }>
-        <Text>Mi nombre es Lillyana Margarita Marrero Caparroso, en la actualidad estudio la carrera de turismo y el ciclo superior de desarollo de apliaciones multiplataforma </Text>
+        <Text style={styles.text}>Mi nombre es Lillyana Margarita Marrero Caparroso, en la actualidad estudio la carrera de turismo y el ciclo superior de desarollo de apliaciones multiplataforma. </Text>
         <QRCode value='https://github.com/LillyanaMarreroCaparroso/1.ContactCard.git'></QRCode>
     </View>
   )
@@ -18,6 +18,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: '100%',
         backgroundColor: '#a25585',
+    },
+    text: {
+        marginHorizontal: 10
     }
 });
 
